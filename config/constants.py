fb502fa36786ebf84ca078042fa32b4348832f42
kgsk758 <yhasutha2@gmail.com>
2026-03-25 20:52:11 +0900

    changed user agent

--- config/constants.py
+++ config/constants.py
@@ -43,7 +43,7 @@ class MenuSiteConfig:
 
 #http request config
 class ScraperConfig:
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     TIMEOUT = 5
 
 class TweetTemplate:
